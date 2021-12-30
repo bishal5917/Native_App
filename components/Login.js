6eb3a8a340bbaebbaf3aa15cf0361c33c499cfeb
Bishal Poudel <poudelb172@gmail.com>
2021-12-30 18:09:59 +0545

    login button done

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React,{useState} from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Login() {
+    const [show,setShow]=useState(false)
+    const onPressLogin=()=>{
+
+    }
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.container}>
+            <Ionicons name="person-circle-sharp" size={94} color="white" />
+            <Text style={styles.login}>Login</Text>
+            <View style={styles.inputCont}>
+                <TextInput style={styles.text} placeholder="Username"></TextInput>
+                <TextInput style={styles.text} placeholder="Password"></TextInput>
+            </View>
+            <Text style={styles.forget}>Forget Password ?</Text>
+            <View style={styles.loginBtn}>
+            <Button
+                onPress={onPressLogin}
+                fontSize="30"
+                color= "teal"
+                title="Submit"
+            />
+            </View> 
         </View>
     )
 }
@@ -12,8 +31,34 @@ export default function Login() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'teal',
-        alignItems: 'center',
+        backgroundColor: '#002633',
+        alignItems: "center",
         justifyContent: 'center',
     },
+    forget: {
+        color: "tomato",
+        margin: 5
+
+    },
+    login: {
+        fontSize: 50,
+        color: "teal"
+    },
+    text: {
+        backgroundColor: "white",
+        padding: 10,
+        fontSize: 20,
+        color: "white",
+        borderRadius: 10,
+        width: 300,
+        margin: 7
+    },
+    inputCont: {
+        marginTop: 50
+    },
+    loginBtn:{
+        width: 315,
+        padding: 10,
+        borderRadius: 10,
+    }
 });
